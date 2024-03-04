fa165480f6de59783be17b93fac4bad172b3b194
Hangyul Yi <hangyulyi3@gmail.com>
2024-03-04 14:58:12 -0700

    format with vsc formatter

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ function Square(props) {
     </button>
   );
 }
- 
+
 class Board extends React.Component {
 
   renderSquare(i) {
@@ -43,8 +43,8 @@ class Board extends React.Component {
     );
   }
 }
- 
- class Game extends React.Component {
+
+class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -71,7 +71,7 @@ class Board extends React.Component {
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
-}
+  }
 
   jumpTo(step) {
     this.setState({
@@ -118,9 +118,9 @@ class Board extends React.Component {
       </div>
     );
   }
- }
+}
 
- function calculateWinner(squares) {
+function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -131,17 +131,16 @@ class Board extends React.Component {
     [0, 4, 8],
     [2, 4, 6],
   ];
-  for (let i = 0; i<lines.length; i++) {
+  for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   }
   return null;
- }
- 
- // ========================================
- 
- const root = ReactDOM.createRoot(document.getElementById("root"));
- root.render(<Game />);
- 
+}
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
